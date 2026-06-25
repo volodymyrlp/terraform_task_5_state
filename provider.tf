5a9bc5ef2f1b6606b3e5eeab305098f1a25438a3
Lysenko Volodymyr <mr.volodumyr@gmail.com>
2026-06-25 22:53:24 +0200

    Solution

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.8.4"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatevlpstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
