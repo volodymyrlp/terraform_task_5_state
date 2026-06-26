4886e4f54327f36bbbf2333477127236d979ff57
Lysenko Volodymyr <mr.volodumyr@gmail.com>
2026-06-26 19:17:27 +0200

    Solution

--- main.tf
+++ main.tf
@@ -8,11 +8,18 @@ terraform {
     }
   }
 
-  backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatevlpstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "this" {
